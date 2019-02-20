8ffad905e221656c064d81dc28bda069d2fa2b7e
SqueakyCheese <james_wooltorton@hotmail.com>
2019-02-20 16:25:43 +0000

    Updating header

--- src/components/common/HeaderAlt.js
+++ src/components/common/HeaderAlt.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
+//import { LinkContainer } from "react-router-bootstrap";
 
 const AltHeader =(props) => {
     return (
         <Navbar default collapseOnSelect fluid>
         <Navbar.Header>
             <Navbar.Brand>
-
                 <Link to="/">
                 <div>
-                <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="" height="20"/>
+                {/* <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" alt="" height="20"/> */}
                 <span>Relent</span>
-                    </div>
+                </div>
                 </Link>
 
             </Navbar.Brand>
@@ -33,14 +33,14 @@ const AltHeader =(props) => {
                     <Link to="/about">About</Link>
                 </NavItem>
                 </Nav>
+
                 <Nav pullRight>
-                <NavItem >
-                <Link to="/login">Login</Link>
+                <NavItem activeclass="active">
+                    <Link to="/login">Login</Link>
                 </NavItem>
                 </Nav>
             </Navbar.Collapse>
-            
-                </Navbar>
+            </Navbar>
     );
 
 
