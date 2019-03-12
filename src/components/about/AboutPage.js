de4b2807a0d5f69e9adf9ff9f35c04bd9a96f6b8
SqueakyCheese <james_wooltorton@hotmail.com>
2019-03-12 20:24:07 +0000

    fixed refresh bug

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -8,7 +8,7 @@ const AboutPage =() => {
         <div>
             <Jumbotron>
                 <h4>About me:</h4>
-                <p>I'm a freelance developer and digital nomad, currently living and working in...<s> London</s>, <s>Berlin</s>, <s>Barcelona</s>, Chamonix</p>
+                <p>I'm a freelance software engineer and digital nomad. Currently living and working in...<s> London</s>, <s>Berlin</s>, <s>Barcelona</s>, Chamonix</p>
                 <p>Feel free to <a href="mailto:james_wooltorton@hotmail.com">email</a> me with feedback/comments/suggestions.</p>
                 {/* <Map /> */}
 
